8a5862a86fbb2d8eb7171eb052e5519e5a61b3bb
Joe Snyder <snyderjosephd@yahoo.com>
2020-12-02 19:55:01 -0600

    Add a supervisor to the dictionary process

--- dictionary/lib/dictionary/application.ex
+++ dictionary/lib/dictionary/application.ex
@@ -4,7 +4,19 @@ defmodule Dictionary.Application do
 
   # Don't want to link application to overall runtime, so just use start(), not start_link()
   def start(_type, _args) do
-    Dictionary.WordList.start_link()
+
+    import Supervisor.Spec
+
+    children = [
+      worker(Dictionary.WordList, [])
+    ]
+
+    options = [
+      name: Dictionary.Supervisor,
+      strategy: :one_for_one
+    ]
+
+    Supervisor.start_link(children, options)
   end
 
 end
