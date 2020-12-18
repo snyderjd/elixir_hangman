da0ff664decbc277f680bc6eab3b751329b40a84
Joe Snyder <snyderjosephd@yahoo.com>
2020-12-18 09:26:48 -0600

    Add gallows svg to view

--- gallows/lib/gallows_web/views/hangman_view.ex
+++ gallows/lib/gallows_web/views/hangman_view.ex
@@ -1,7 +1,21 @@
 defmodule GallowsWeb.HangmanView do
   use GallowsWeb, :view
+  import Gallows.Views.Helpers.GameStateHelper
 
-  def display_word(tally) do
-    tally.letters |> Enum.join(" ")
+  def turn(left, target) when target >= left do
+    "opacity: 1"
   end
+
+  def turn(_left, _target) do
+    "opacity: 0.1"
+  end
+
+  def new_game_button(conn) do
+    button("New Game", to: Routes.hangman_path(conn, :create_game))
+  end
+
+  def game_over?(%{ game_state: game_state }) do
+    game_state in [ :won, :lost ]
+  end
+
 end
